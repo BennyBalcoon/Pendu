8db2da6c45b26ff20933c566b135b6e66e1a839f
BennyBalcoon <benoitbalcon@hotmail.fr>
2019-10-14 16:13:52 +0200

    create new component to generate the word to guess

---
+++ src/components/letterToGuess/letterToGuess.component.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './letterToGuess.styles.css'
+
+function randomWord(words) {
+    return words[Math.floor(Math.random()*words.length)];
+}
+
+const words = [
+    'bonjour',
+    'parapluie',
+    'buzz',
+    'chocolat',
+    'ornithorynque',
+    'hortensia',
+    'jardinier'
+];
+
+const wordToGuess = randomWord(words);
+const wordSplitted = wordToGuess.split("");
+console.log(wordSplitted);
+
+const HIDDEN_SYMBOL = '❓'
+
+const Test = ({feedback}) => (
+    <div>
+        {wordSplitted.map(letter => (
+            <span className="symbol">
+                {feedback === 'hidden' ? HIDDEN_SYMBOL : letter}
+            </span>
+        ))}
+    </div>
+)
+
+export default Test;
+
---
+++ src/components/wordToGuess/wordToGuess.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Test from '../letterToGuess/letterToGuess.component';
+
+export const WordToGuess = (props) => (
+    <div>
+     <Test feedback="hidden" />
+    </div>
+);

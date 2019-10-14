24537b390cb4f262d5e5dd7b0a69b60a7d3c7216
BennyBalcoon <benoitbalcon@hotmail.fr>
2019-10-14 16:11:59 +0200

    add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import {Keyboard} from './components/keyboard/keyboard.component';
+import { Keyboard } from "./components/keyboard/keyboard.component";
+import {WordToGuess} from "./components/wordToGuess/wordToGuess.component"
 
 
 class App extends Component {
@@ -9,32 +10,48 @@ class App extends Component {
 
     this.state = {
       letters: [
-        {name: "a", id: 1},
-        {name: "z", id: 2},
-        {name: "e", id: 3},
-        {name: "r", id: 4},
-        {name: "t", id: 5},
-        {name: "y", id: 6},
-        {name: "u", id: 7},
-        {name: "i", id: 8},
-        {name: "o", id: 9},
-        {name: "p", id: 10},
-        {name: "q", id: 11},
-        {name: "s", id: 12},
-        {name: "d", id: 13},
-        {name: "f", id: 14},
-        {name: "g", id: 15},
-        {name: "h", id: 16},
-        {name: "j", id: 17},
-        {name: "k", id: 18},
-        {name: "l", id: 19},
-        {name: "m", id: 20},
+        { name: "a", id: 1 },
+        { name: "z", id: 2 },
+        { name: "e", id: 3 },
+        { name: "r", id: 4 },
+        { name: "t", id: 5 },
+        { name: "y", id: 6 },
+        { name: "u", id: 7 },
+        { name: "i", id: 8 },
+        { name: "o", id: 9 },
+        { name: "p", id: 10 },
+        { name: "q", id: 11 },
+        { name: "s", id: 12 },
+        { name: "d", id: 13 },
+        { name: "f", id: 14 },
+        { name: "g", id: 15 },
+        { name: "h", id: 16 },
+        { name: "j", id: 17 },
+        { name: "k", id: 18 },
+        { name: "l", id: 19 },
+        { name: "m", id: 20 },
+        { name: "w", id: 21 },
+        { name: "x", id: 22 },
+        { name: "c", id: 23 },
+        { name: "v", id: 24 },
+        { name: "b", id: 25 },
+        { name: "n", id: 26 }
+      ],
+      words: [
+        "bonjour",
+        "parapluie",
+        "buzz",
+        "chocolat",
+        "ornythorinque",
+        "hortensia",
+        "jardinier"
       ]
     };
   }
   render() {
     return (
       <div className="App">
+        <WordToGuess />
         <Keyboard letters={this.state.letters} />
       </div>
     );
